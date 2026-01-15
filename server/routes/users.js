b0997049fd2bac5a10c29645f611494bef3bba54
xyryc <amdanikzz@gmail.com>
2026-01-16 01:16:52 +0600

    error handling

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,18 @@
-const express = require('express');
+const express = require("express");
 const User = require("../models/User");
-const authMiddleware = require('../middleware/auth');
+const authMiddleware = require("../middleware/auth");
 
 const router = express.Router();
 
-router.get("/all", authMiddleware, async(req, res)=> {
-    const users = await User.find({_id: {$ne: req.userId} }).select("name email")
-    res.json(users)
-})
+router.get("/all", authMiddleware, async (req, res) => {
+  try {
+    const users = await User.find({ _id: { $ne: req.userId } }).select(
+      "name email"
+    );
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-module.exports = router
+module.exports = router;
