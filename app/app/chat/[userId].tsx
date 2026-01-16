ce47cbbffb430ca150d628f5e018b941985161e0
xyryc <amdanikzz@gmail.com>
2026-01-16 06:32:12 +0600

    send and receive message

--- app/app/chat/[userId].tsx
+++ app/app/chat/[userId].tsx
@@ -1,22 +1,32 @@
-import { View, Text, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { useLocalSearchParams } from "expo-router";
 import {
   useGetMessagesQuery,
   useGetOrCreateConversationMutation,
 } from "@/store/api";
+import { socket } from "@/utils/socket";
 
 const UserId = () => {
   const { userId } = useLocalSearchParams();
-  console.log("chat screen", userId);
+  // console.log("chat screen", userId);
   const [conversationId, setConversationId] = useState(null);
+  const [message, setMessage] = useState("");
+  const [localMessages, setLocalMessages] = useState([]);
 
   const [getConversation, { isLoading: loadingConversation }] =
     useGetOrCreateConversationMutation();
-  const { data: messages, isLoading: loadingMessages } = useGetMessagesQuery(
-    conversationId,
-    { skip: !conversationId }
-  );
+  const {
+    data: messages,
+    isLoading: loadingMessages,
+    error,
+  } = useGetMessagesQuery(conversationId, { skip: !conversationId });
 
   useEffect(() => {
     getConversation(userId)
@@ -25,18 +35,78 @@ const UserId = () => {
       .catch((err) => console.error(err));
   }, [userId]);
 
+  // join conversation room
+  useEffect(() => {
+    if (conversationId) {
+      socket.emit("join_conversation", conversationId);
+      console.log("📥 Joined conversation", conversationId);
+    }
+  }, [conversationId]);
+
+  // sync api messages to local state
+  useEffect(() => {
+    if (messages) {
+      setLocalMessages(messages);
+    }
+  }, [messages]);
+
+  // listen for new messages
+  useEffect(() => {
+    if (!conversationId) return;
+
+    const handleNewMessage = (newMessage) => {
+      console.log("📨 Received: ", newMessage);
+      setLocalMessages((prev) => [...prev, newMessage]);
+    };
+
+    socket.on("receive_message", handleNewMessage);
+
+    return () => {
+      socket.off("receive_message", handleNewMessage);
+    };
+  }, [conversationId]);
+
+  const handleSend = () => {
+    if (!message.trim() || !conversationId) return;
+
+    const messageData = {
+      conversationId,
+      content: message.trim(),
+      type: "text",
+    };
+
+    socket.emit("send_message", messageData);
+    console.log("📥 Sent: ", messageData);
+
+    setMessage("");
+  };
+
   return (
-    <View>
+    <View className="mx-5 flex-1">
       <Text>Chat - Conversation: {conversationId}</Text>
 
       <FlatList
-        data={messages || []}
+        data={localMessages}
+        keyExtractor={(item) => item._id}
         renderItem={({ item }) => (
           <Text>
             {item.sender.name}: {item.content}
           </Text>
         )}
       />
+
+      <View className="flex-row mb-10">
+        <TextInput
+          value={message}
+          onChangeText={setMessage}
+          placeholder="Type Message"
+          className="flex-1 p-2.5 border bg-gray-200"
+        />
+
+        <TouchableOpacity onPress={handleSend} className="bg-black p-2.5">
+          <Text className="text-white">Send</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
--- app/store/api.ts
+++ app/store/api.ts
@@ -10,6 +10,7 @@ export const api = createApi({
       return headers;
     },
   }),
+  tagTypes: ["Meesages"]
 
   endpoints: (builder) => ({
     register: builder.mutation({
@@ -43,6 +44,7 @@ export const api = createApi({
     getMessages: builder.query({
       query: (conversationId) =>
         `/message/conversation/${conversationId}/messages`,
+      providesTags: ["Meesages"]
     }),
   }),
 });
